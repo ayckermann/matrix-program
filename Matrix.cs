259b297bd542019f9f86519c160a827411acdd36
ayckermann <aya.mf12@gmail.com>
2022-11-16 18:57:14 +0700

    First commit

---
+++ Matrix.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace matriks
+{
+    public class Matrix
+    {
+        public int x,y;
+        public int [,] value;
+
+        public Matrix(int x, int y){
+            this.x = x;
+            this.y = y;
+            this.value = new int[x,y];
+        }
+        public Matrix(int x, int y, int[,] value){
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+
+        public void display()
+        {
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){
+                    Console.Write(value[i,j]+" ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public Matrix tambahMatrix(Matrix b){
+            Matrix hasil = new Matrix(x,y);
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){
+                    hasil.value[i,j] = value[i,j] + b.value[i,j];
+                }
+            }
+            return hasil;
+        }
+
+        public Matrix kurangMatrix(Matrix b){
+            Matrix hasil = new Matrix(x,y);
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){
+                    hasil.value[i,j] = value[i,j] - b.value[i,j];
+                }
+            }
+            return hasil;
+        }
+    
+        public Matrix kaliMatrix(Matrix b){
+            if(y == b.x){
+                Matrix hasil = new Matrix(x,b.y);
+                for (int i = 0; i < x; i++) {
+                    for (int j =0; j < b.y; j++){
+                        for (int k = 0; k < y; k++){
+                            hasil.value[i,j] += value[i,k] * b.value[k,j];
+                            
+                        }
+                    }
+                }
+                return hasil;
+            }
+            else{
+                throw new ArgumentOutOfRangeException(
+                    "Gagal! Dimensi tidak cocok");
+            }
+        }
+        public Matrix kaliKoefisien(int n){
+            Matrix hasil = new Matrix(x,y);
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){
+                    hasil.value[i,j] = value[i,j] * n;
+                }
+            }
+            return hasil;
+        }
+        public Matrix transpose(){
+            Matrix hasil = new Matrix(y,x);
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){
+                    hasil.value[j,i] = value[i,j];
+                   
+                }
+            }
+            return hasil;
+        }
+
+        public Matrix minorMatrix(int a, int b){
+            Matrix hasil = new Matrix(x-1,y-1);
+            int a1 =0, b1=0;
+         
+            for (int i = 0; i < x; i++) {
+                for (int j =0; j < y; j++){ 
+                    if(i != a-1 && j != b-1){
+                        if(b1<y-1){
+                            hasil.value[a1,b1] = value[i,j];
+                            b1++;
+                        } 
+                        if(b1==y-1){
+                            b1=0;
+                            a1++;   
+                        }
+                    }           
+                }
+            }
+    
+            return hasil;
+        }
+        public int detMinor(int a, int b){
+            Matrix temp = minorMatrix(a,b);
+            int hasil = temp.determinan();
+            return hasil;
+
+        }
+        public Matrix kofaktor(){
+            Matrix hasil = new Matrix(x,y);
+            for(int i =0;i < x;i++){
+                for(int j =0 ; j < y;j++){
+                    if(i%2 ==0 && j%2 !=0){
+                        hasil.value[i,j] = -1 * detMinor(i+1,j+1);
+                    }
+                    else if(i%2!=0 && j%2==0){
+                        hasil.value[i,j] = -1 * detMinor(i+1,j+1);
+                    }
+                    else{
+                        hasil.value[i,j] =detMinor(i+1,j+1);
+                    }
+                }
+            }
+            return hasil;
+            
+        }
+        public Matrix adjoin(){
+            Matrix hasil = kofaktor();
+            return hasil.transpose();
+
+        }
+
+        public Matrix invers(){
+            Matrix hasil = adjoin();
+            int det = determinan();
+
+            return hasil.kaliKoefisien(1/det);;
+        }
+        public int determinan(){
+            if(x==2 && y==2){
+                int hasil = (value[0,0] * value[1,1]) - (value[0,1]*value[1,0]) ; 
+                return hasil;
+            }
+            else if(x==3 && y==3){
+                int hasil =0;
+                hasil = ((value[0,0] * value[1,1] * value[2,2]) + 
+                        (value[0,1] * value[1,2] * value[2,0]) +
+                        (value[0,2] * value[1,0] * value[2,1])) - 
+                        ((value[2,0] * value[1,1] * value[0,2]) +
+                        (value[2,1] * value[1,2] * value[0,0]) +
+                        (value[2,2] * value[1,0] * value[0,1]))
+                ;
+                return hasil;
+            }
+            else if(x==4 && y==4){
+                int hasil;
+                hasil = (value[0,0] * detMinor(1,1)) -
+                        (value[0,1] * detMinor(1,2)) +
+                        (value[0,2] * detMinor(1,3)) -
+                        (value[0,3] * detMinor(1,4));
+
+                return hasil;
+                
+            }
+           
+            else{
+                 throw new ArgumentOutOfRangeException(
+                    "Gagal! Dimensi tidak cocok");
+            }
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using matriks;
+using static matriks.Matrix;
+
+int[,] x = new int[3,2] {{1,2},{2,3},{1,4}};
+int[,] y = new int[2,3] {{1,2,2},{2,3,1}};
+int[,] z = new int[2,2] {{1,3},{4,8}};
+int[,] a3 = new int[3,3] {{1,2,2},{2,3,1},{1,4,6}};
+int[,] a4 = new int[4,4] {{1,2,3,4},{1,5,3,2},{1,3,5,2},{4,2,1,5}}; 
+Matrix a = new Matrix(3,2,x);
+Matrix b = new Matrix(2,3,y);
+Matrix c = a.kaliMatrix(b);
+Matrix d = new Matrix(a.x,b.y);
+Matrix e = a.transpose();
+Matrix f = a.kaliKoefisien(2);
+Matrix g = new Matrix(3,3,a3);
+int h = g.determinan();
+Matrix i = g.minorMatrix(3,3);
+int j  = g.detMinor(3,3);
+Matrix k = new Matrix(4,4,a4);
+
+int l = k.determinan();
+Matrix m = g.invers();
+
+Console.Write("Matrix \n");
+g.display();    
+Console.Write(" \n");
+m.display();
+Console.Write("\n");
+// Console.Write(j);
+// Console.Write(l);
